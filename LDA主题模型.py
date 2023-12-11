af8801acef87e6ca6ea7d3054227085c802e3bea
wztsir <1541529408@qq.com>
2023-12-11 20:10:00 +0800

    choose the best topic numbers

--- LDA主题模型.py
+++ LDA主题模型.py
@@ -1,32 +1,15 @@
-# import pyLDAvis.gensim
-import pyLDAvis.sklearn
-import pyLDAvis
-import numpy as np
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import LatentDirichletAllocation
-import pandas as pd
-import jieba
-import re
 import os
-
-# 待做 LDA 的文本 csv 文件，可以是本地文件，也可以是远程文件，一定要保证它是存在的！！！！
-# source_csv_path = 'comment.csv'
-source_excel_path = 'data/2020 lda.xlsx'
-# 文本 csv 文件里面文本所处的列名,注意这里一定要填对，要不然会报错的！！！
-document_column_name = 'content'
-# 输出主题词的文件路径
-top_words_csv_path = 'top-topic-words.csv'
-# 输出各文档所属主题的文件路径
-predict_topic_csv_path = 'document-distribution.csv'
-# 可视化 html 文件路径
-html_path = 'document-lda-visualization.html'
-# 选定的主题数
-n_topics = 3
-# 要输出的每个主题的前 n_top_words 个主题词数
-n_top_words = 15
-# 去除无意义字符的正则表达式
-pattern = u'[\\s\\d,.<>/?:;\'\"[\\]{}()\\|~!\t"@#$%^&*\\-_=+，。\n《》、？：；“”‘’｛｝【】（）…￥！—┄－]+'
+import re
+import jieba
+import numpy as np
+import pandas as pd
+from multiprocessing import freeze_support
+import gensim
+from gensim.models.ldamodel import LdaModel
+import pyLDAvis.sklearn
+from gensim.models import CoherenceModel
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def top_words_data_frame(model: LatentDirichletAllocation,
@@ -76,63 +59,127 @@ def predict_to_data_frame(model: LatentDirichletAllocation, X: np.ndarray) -> pd
     return df
 
 
-df = (
-    pd.read_excel(
-        source_excel_path)
-        .drop_duplicates()
-        .rename(columns={
-        document_column_name: 'text'
-    }))
-# 设置停用词集合
-stop_words_set = set(
-    ['你', '我', 'nan', '的', '什么', '怎么', '微博', '转发', '可以', '就是', '自己', '我们', '他们', '你们', '这个', '应该', '现在', '核子', '真的',
-     '一边', '如何', '不到', '决定', '意思', '看待', '试试', '各位', '查看'])
-# 去重、去缺失、分词
-df['cut'] = (
-    df['text']
-        .apply(lambda x: str(x))
-        .apply(lambda x: re.sub(pattern, ' ', x))
-        .apply(lambda x: " ".join([word for word in jieba.lcut(x) if word not in stop_words_set]))
-)
+def calculate_coherence_score(dictionary, corpus, texts, limit, start=2, step=1):
+    coherence_values = []
+    model_list = []
+    for num_topics in range(start, limit, step):
+        # 使用 LdaModel 替代 LatentDirichletAllocation
+        lda_model = LdaModel(
+            corpus=corpus,
+            id2word=dictionary,
+            num_topics=num_topics,
+            iterations=50  # 可根据需要调整
+        )
+        model_list.append(lda_model)
+        coherence_model = CoherenceModel(
+            model=lda_model, texts=texts, dictionary=dictionary, coherence='c_v')
+        coherence_values.append(coherence_model.get_coherence())
 
-# 构造 tf-idf
-tf_idf_vectorizer = TfidfVectorizer()
-tf_idf = tf_idf_vectorizer.fit_transform(df['cut'])
+    return model_list, coherence_values
 
-lda = LatentDirichletAllocation(
-    n_components=n_topics,
-    max_iter=50,
-    learning_method='online',
-    learning_offset=50,
-    random_state=0)
 
-# 使用 tf_idf 语料训练 lda 模型
-lda.fit(tf_idf)
+def main():
+    # 待做 LDA 的文本 csv 文件，可以是本地文件，也可以是远程文件，一定要保证它是存在的！！！！
+    # source_csv_path = 'comment.csv'
+    source_excel_path = 'data/2022 lda.xlsx'
+    # 文本 csv 文件里面文本所处的列名,注意这里一定要填对，要不然会报错的！！！
+    document_column_name = 'content'
+    # 输出主题词的文件路径
+    top_words_csv_path = 'res/2022/top-topic-words.csv'
+    # 输出各文档所属主题的文件路径
+    predict_topic_csv_path = 'res/2022/document-distribution.csv'
+    # 可视化 html 文件路径
+    html_path = 'res/2022/document-lda-visualization.html'
+    # 选定的主题数
+    # n_topics = 3
+    # 要输出的每个主题的前 n_top_words 个主题词数
+    n_top_words = 15
+    # 去除无意义字符的正则表达式
+    pattern = u'[\\s\\d,.<>/?:;\'\"[\\]{}()\\|~!\t"@#$%^&*\\-_=+，。\n《》、？：；“”‘’｛｝【】（）…￥！—┄－]+'
+    df = (
+        pd.read_excel(
+            source_excel_path)
+            .drop_duplicates()
+            .rename(columns={
+            document_column_name: 'text'
+        }))
+    # 设置停用词集合
+    stop_words_set = set(
+        ['你', '我', 'nan', '的', '什么', '怎么', '微博', '转发', '可以', '就是', '自己', '我们', '他们', '你们', '这个', '应该', '现在', '核子', '真的',
+         '一边', '如何', '不到', '决定', '意思', '看待', '试试', '各位', '查看'])
+    # 去重、去缺失、分词
+    df['cut'] = (
+        df['text']
+            .apply(lambda x: str(x))
+            .apply(lambda x: re.sub(pattern, ' ', x))
+            .apply(lambda x: " ".join([word for word in jieba.lcut(x) if word not in stop_words_set]))
+    )
 
-# 计算 n_top_words 个主题词
-top_words_df = top_words_data_frame(lda, tf_idf_vectorizer, n_top_words)
+    # 构造 tf-idf
+    tf_idf_vectorizer = TfidfVectorizer()
+    tf_idf = tf_idf_vectorizer.fit_transform(df['cut'])
 
-# 保存 n_top_words 个主题词到 csv 文件中
-top_words_df.to_csv(top_words_csv_path, encoding='utf-8-sig', index=None)
+    # 准备文本数据
+    text_data = df['cut'].apply(lambda x: x.split())
 
-# 转 tf_idf 为数组，以便后面使用它来对文本主题概率分布进行计算
-X = tf_idf.toarray()
+    # 创建字典和语料库
+    dictionary = gensim.corpora.Dictionary(text_data)
+    corpus = [dictionary.doc2bow(text) for text in text_data]
 
-# 计算完毕主题概率分布情况
-predict_df = predict_to_data_frame(lda, X)
+    # 设置计算一致性分数的范围
+    start, limit, step = 2, 10, 1
+    # 计算一致性分数
+    freeze_support()  # 添加这行代码
+    model_list, coherence_values = calculate_coherence_score(
+       dictionary, corpus, text_data, limit, start, step)
 
-# 保存文本主题概率分布到 csv 文件中
-predict_df.to_csv(predict_topic_csv_path, encoding='utf-8-sig', index=None)
+    # 选择具有最高一致性分数的模型
+    best_model_index = np.argmax(coherence_values)
+    best_model = model_list[best_model_index]
+    best_num_topics = start + best_model_index * step
 
-# 使用 pyLDAvis 进行可视化
-data = pyLDAvis.sklearn.prepare(lda, tf_idf, tf_idf_vectorizer)
-pyLDAvis.save_html(data, html_path)
-# 清屏
-os.system('clear')
-# 浏览器打开 html 文件以查看可视化结果
-os.system(f'start {html_path}')
+    # 输出最佳主题数
+    print(f"Best Number of Topics: {best_num_topics}")
 
-print('本次生成了文件：',
-      top_words_csv_path,
-      predict_topic_csv_path,
-      html_path)
+    # 使用最佳主题数建立模型
+    lda = LatentDirichletAllocation(
+        n_components=best_num_topics,
+        max_iter=50,
+        learning_method='online',
+        learning_offset=50,
+        random_state=0)
+
+    # 使用 tf_idf 语料训练 lda 模型
+    lda.fit(tf_idf)
+
+    # 计算 n_top_words 个主题词
+    top_words_df = top_words_data_frame(lda, tf_idf_vectorizer, n_top_words)
+
+    # 保存 n_top_words 个主题词到 csv 文件中
+    top_words_df.to_csv(top_words_csv_path, encoding='utf-8-sig', index=None)
+
+    # 转 tf_idf 为数组，以便后面使用它来对文本主题概率分布进行计算
+    X = tf_idf.toarray()
+
+    # 计算完毕主题概率分布情况
+    predict_df = predict_to_data_frame(lda, X)
+
+    # 保存文本主题概率分布到 csv 文件中
+    predict_df.to_csv(predict_topic_csv_path, encoding='utf-8-sig', index=None)
+
+    # 使用 pyLDAvis 进行可视化
+    data = pyLDAvis.sklearn.prepare(lda, tf_idf, tf_idf_vectorizer)
+    pyLDAvis.save_html(data, html_path)
+    # 清屏
+    os.system('clear')
+    # 浏览器打开 html 文件以查看可视化结果
+    os.system(f'start {html_path}')
+
+    print('本次生成了文件：',
+          top_words_csv_path,
+          predict_topic_csv_path,
+          html_path)
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # LDA
+
+##环境配置
+环境在终端中运行
+
+```
+pip install -r requirements.txt(修改为对应路径)
+```
+
+##html阅读
+在 pyLDAvis 主题可视化中，"Overall term frequency"（整体词频）和"Estimated term frequency within the selected topic"（在选定主题中的估计词频）是两个用于衡量主题关键词的重要性的指标。
+
+###Overall Term Frequency（整体词频）：
+
+含义： 衡量了语料库中所有文档中某个词语的总出现频率。
+作用： 该值越高，表示该词在整个语料库中的使用越频繁。在主题模型中，这有助于确定哪些词在整个语料库中具有重要性。
+###Estimated Term Frequency within the Selected Topic（在选定主题中的估计词频）：
+
+含义： 衡量了某个词在选定的主题中的相对重要性。它是根据 LDA 模型中的概率分布计算得出的。
+作用： 该值越高，表示该词在选定的主题中更为重要。在理解主题关键词时，这个指标可以帮助确定哪些词在给定主题中是核心的、有代表性的。
+
+###Slide to adjust relevance metric(滑块)
+在 pyLDAvis 中，通过滑块（Slide to adjust relevance metric）来调整关键词的相关性度量（relevance metric）。这个滑块控制了公式中的 λ 参数，即权衡两部分的相对重要性。
+
+在具体的影响上，调整这个滑块可以改变可视化结果中关键词的显示方式。具体来说，关键词的大小和颜色是由相关性度量计算得到的，因此改变 λ 参数会调整关键词在可视化中的重要性。
+
+当 λ 接近 0 时，关键词的显示受到主题中的概率分布较大的影响。
+当 λ 接近 1 时，关键词的显示受到在整个语料库中的概率分布的影响更大。
